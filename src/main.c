de2819c3b2b70f20e1604b7da8a26ee2c84452a3
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-12-13 16:05:08 +0100

    image

--- src/main.c
+++ src/main.c
@@ -6,30 +6,49 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 10:33:52 by anfouger          #+#    #+#             */
-/*   Updated: 2025/12/13 15:01:02 by anfouger         ###   ########.fr       */
+/*   Updated: 2025/12/13 15:40:50 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fractol.h>
 
+void put_pixel(char *addr, int line_len, int bpp, int x, int y, int color)
+{
+    char *pixel;
+
+    pixel = addr + (y * line_len + x * (bpp / 8));
+    *(unsigned int *)pixel = color;
+}
+
 int main(void)
 {
-	void	*mlx;
-	void	*win;
-	int		i;
-	int		j;
+    void *mlx;
+    void *win;
+    void *img;
+    char *addr;
+    int   bpp;
+    int   line_len;
+    int   endian;
+    int   x;
+    int   y;
 
-	i = 0;
-	j = 0;
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, 800, 600, "MLX test");
-	while (i++ < 10)
-	{
-		j = 0;
-		while (j++ < 10)
-			mlx_pixel_put(mlx, win, 200 + j, 150 + i, 0x00ff00);
-	}
-	ft_printf("MLX ready!\n");
-	mlx_loop(mlx);
-	return (0);
+    mlx = mlx_init();
+    win = mlx_new_window(mlx, 800, 600, "Image MLX");
+
+    img = mlx_new_image(mlx, 400, 300);
+    addr = mlx_get_data_addr(img, &bpp, &line_len, &endian);
+    y = 0;
+    while (y < 300)
+    {
+        x = 0;
+        while (x < 400)
+        {
+            put_pixel(addr, line_len, bpp, x, y, 0x0000FF);
+            x++;
+        }
+        y++;
+    }
+    mlx_put_image_to_window(mlx, win, img, 0, 0);
+    mlx_loop(mlx);
 }
+
