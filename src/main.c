67fea2f774b94a8166ae2ba89e7f30f389d17008
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-12-13 15:01:32 +0100

    draw a square

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 10:33:52 by anfouger          #+#    #+#             */
-/*   Updated: 2025/12/13 14:46:33 by anfouger         ###   ########.fr       */
+/*   Updated: 2025/12/13 15:01:02 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,19 @@ int main(void)
 {
 	void	*mlx;
 	void	*win;
+	int		i;
+	int		j;
 
+	i = 0;
+	j = 0;
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 800, 600, "MLX test");
+	while (i++ < 10)
+	{
+		j = 0;
+		while (j++ < 10)
+			mlx_pixel_put(mlx, win, 200 + j, 150 + i, 0x00ff00);
+	}
 	ft_printf("MLX ready!\n");
 	mlx_loop(mlx);
 	return (0);
