f2eb1ae2ed2b9a153918789fce1fdb2c0186c54d
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-12-17 11:16:48 +0100

    clean exit

--- src/main.c
+++ src/main.c
@@ -6,44 +6,58 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 10:33:52 by anfouger          #+#    #+#             */
-/*   Updated: 2025/12/17 09:19:12 by anfouger         ###   ########.fr       */
+/*   Updated: 2025/12/17 11:16:28 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fractol.h>
 
-int	key_pressed(int keycode, void *param)
+typedef struct s_data
 {
-	(void)param;
-	write(1, "key press\n", 15);
+	void	*win;
+	void	*mlx;
+} t_data;
+
+int	key_pressed(int keycode, t_data *param)
+{
+	ft_printf("key press\n");
 	if (keycode == 65307)
+	{
+		mlx_destroy_window(param->mlx, param->win);
+		mlx_destroy_display(param->mlx);
+		free(param->mlx);
+		free(param);
 		exit(0);
+	}
 	return (0);
 }
 
-int	ft_exit(void *param)
+int	ft_exit(t_data *param)
 {
-	(void)param;
+	mlx_destroy_window(param->mlx, param->win);
+	mlx_destroy_display(param->mlx);
+	free(param->mlx);
+	free(param);
 	exit(0);
 	return(0);    
 }
 
 int	main(void)
 {
-	void	*mlx;
-	void	*win;
+	t_data *test;
 
-	mlx = mlx_init();
+	test = malloc(sizeof(t_data));
+	test->mlx = mlx_init();
     
-	if (!mlx)
+	if (!test->mlx)
 		return (1);
 
-	win = mlx_new_window(mlx, 800, 600, "mlx_hook test");
-	if (!win)
+	test->win = mlx_new_window(test->mlx, 1080, 720, "mlx_hook test");
+	if (!test->win)
 		return (1);
-	mlx_hook(win, 2, 1L << 0, key_pressed, NULL);
-	mlx_hook(win, 17, 0, ft_exit, NULL);
-	mlx_loop(mlx);
+	mlx_hook(test->win, 2, 1L << 0, key_pressed, test);
+	mlx_hook(test->win, 17, 0, ft_exit, test);
+	mlx_loop(test->mlx);
 	return (0);
 }
 
