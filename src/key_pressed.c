eb1913899d2b5b825ac78c5e4170f7864037a5ce
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-12-22 10:05:50 +0100

    code verbosed for key_pressed

--- src/key_pressed.c
+++ src/key_pressed.c
@@ -6,7 +6,7 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 15:28:35 by anfouger          #+#    #+#             */
-/*   Updated: 2025/12/22 09:58:04 by anfouger         ###   ########.fr       */
+/*   Updated: 2025/12/22 10:03:52 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 static void	change_palette(int keycode, t_data *data)
 {
-	if (keycode == 49)
+	if (keycode == '1')
 	{
 		if (data->palette == 0)
 			data->palette = 8;
 		else
 			data->palette--;	
 	}
-	else if (keycode == 50)
+	else if (keycode == '2')
 	{
 		if (data->palette == 8)
 			data->palette = 0;
@@ -32,26 +32,26 @@ static void	change_palette(int keycode, t_data *data)
 
 static void	change_julia(int keycode, t_data *data)
 {
-	if (keycode == 99)
+	if (keycode == 'c')
 		data->julia_im -= 0.001;	
-	else if (keycode == 118)
+	else if (keycode == 'v')
 		data->julia_im += 0.001;
-	else if (keycode == 122)
+	else if (keycode == 'z')
 		data->julia_re -= 0.001;
-	else if (keycode == 120)
+	else if (keycode == 'x')
 		data->julia_re += 0.001;
 }
 
 static void	change_fractal(int keycode, t_data *data)
 {
-	if (keycode == 51)
+	if (keycode == '3')
 	{
 		if (data->fractal == 0)
 			data->fractal = 1;
 		else
 			data->fractal--;	
 	}
-	else if (keycode == 52)
+	else if (keycode == '4')
 	{
 		if (data->fractal == 1)
 			data->fractal = 0;
@@ -62,13 +62,13 @@ static void	change_fractal(int keycode, t_data *data)
 
 static void	move(int keycode, t_data *data)
 {
-	if (keycode == 65361 || keycode == 97)
+	if (keycode == 65361 || keycode == 'a')
 		data->offset_x -= 0.4 / data->zoom;
-	else if (keycode == 65363 || keycode == 100)
+	else if (keycode == 65363 || keycode == 'd')
 		data->offset_x += 0.4 / data->zoom;
-	else if (keycode == 65362 || keycode == 119)
+	else if (keycode == 65362 || keycode == 'w')
 		data->offset_y -= 0.4 / data->zoom;																	
-	else if (keycode == 65364 || keycode == 115)
+	else if (keycode == 65364 || keycode == 's')
 		data->offset_y += 0.4 / data->zoom;
 }
 
@@ -76,15 +76,15 @@ int	ft_key_pressed(int keycode, t_data *data)
 {
 	if (keycode == 65307)
 		ft_exit(data);
-	else if ((keycode >= 65361 && keycode <= 65364) || keycode == 119 
-		|| keycode == 97 || keycode == 115 || keycode == 100)
+	else if ((keycode >= 65361 && keycode <= 65364) || keycode == 'w' 
+		|| keycode == 'a' || keycode == 's' || keycode == 'd')
 		move(keycode, data);
-	else if (keycode == 49 || keycode == 50)
+	else if (keycode == '1' || keycode == '2')
 		change_palette(keycode, data);
-	else if (keycode == 51 || keycode == 52)
+	else if (keycode == '3' || keycode == '4')
 		change_fractal(keycode, data);
-	else if (keycode == 122 || keycode == 120 
-		|| keycode == 99 || keycode == 118)
+	else if (keycode == 'z' || keycode == 'x' 
+		|| keycode == 'c' || keycode == 'v')
 		change_julia(keycode, data);
 	render(data);
 	return (0);
