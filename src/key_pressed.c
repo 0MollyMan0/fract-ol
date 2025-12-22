765356bc6cc63822bbcb8942d962f9f57cd83c86
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-12-22 10:00:13 +0100

    the user can change the real number of Julia fractal whith key

--- src/key_pressed.c
+++ src/key_pressed.c
@@ -6,7 +6,7 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 15:28:35 by anfouger          #+#    #+#             */
-/*   Updated: 2025/12/21 16:46:20 by anfouger         ###   ########.fr       */
+/*   Updated: 2025/12/22 09:58:04 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,14 @@ static void	change_palette(int keycode, t_data *data)
 
 static void	change_julia(int keycode, t_data *data)
 {
-	if (keycode == 113)
+	if (keycode == 99)
 		data->julia_im -= 0.001;	
-	else if (keycode == 101)
+	else if (keycode == 118)
 		data->julia_im += 0.001;
+	else if (keycode == 122)
+		data->julia_re -= 0.001;
+	else if (keycode == 120)
+		data->julia_re += 0.001;
 }
 
 static void	change_fractal(int keycode, t_data *data)
@@ -58,13 +62,13 @@ static void	change_fractal(int keycode, t_data *data)
 
 static void	move(int keycode, t_data *data)
 {
-	if (keycode == 65361)
+	if (keycode == 65361 || keycode == 97)
 		data->offset_x -= 0.4 / data->zoom;
-	else if (keycode == 65363)
+	else if (keycode == 65363 || keycode == 100)
 		data->offset_x += 0.4 / data->zoom;
-	else if (keycode == 65362)
+	else if (keycode == 65362 || keycode == 119)
 		data->offset_y -= 0.4 / data->zoom;																	
-	else if (keycode == 65364)
+	else if (keycode == 65364 || keycode == 115)
 		data->offset_y += 0.4 / data->zoom;
 }
 
@@ -72,13 +76,15 @@ int	ft_key_pressed(int keycode, t_data *data)
 {
 	if (keycode == 65307)
 		ft_exit(data);
-	else if (keycode >= 65361 && keycode <= 65364)
+	else if ((keycode >= 65361 && keycode <= 65364) || keycode == 119 
+		|| keycode == 97 || keycode == 115 || keycode == 100)
 		move(keycode, data);
 	else if (keycode == 49 || keycode == 50)
 		change_palette(keycode, data);
 	else if (keycode == 51 || keycode == 52)
 		change_fractal(keycode, data);
-	else if (keycode == 113 || keycode == 101)
+	else if (keycode == 122 || keycode == 120 
+		|| keycode == 99 || keycode == 118)
 		change_julia(keycode, data);
 	render(data);
 	return (0);
